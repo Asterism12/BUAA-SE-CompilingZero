66b6fa7d9b94e6c9832983fb48dd6443480adb1f
Qsort <awpfriend@sina.com>
2019-11-30 00:57:22 +0800

    update help file

--- help/help.cpp
+++ help/help.cpp
@@ -1,4 +1,17 @@
 #include "help.h"
 void getHelp() {
-	std::cout << "It's a help file" << std::endl;
+	std::string str = R"(
+Usage:
+  cc0 [options] input [-o file]
+or 
+  cc0 [-h]
+Options:
+  -s        将输入的 c0 源代码翻译为文本汇编文件
+  -c        将输入的 c0 源代码翻译为二进制目标文件
+  -h        显示关于编译器使用的帮助
+  -o file   输出到指定的文件 file
+
+不提供任何参数时，默认为 -h
+提供 input 不提供 -o file 时，默认为 -o out)";
+	std::cout << str << std::endl;
 }
