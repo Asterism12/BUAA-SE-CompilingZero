e50e75b7df10f131982e254af3e3cda381e4e029
Qsort <awpfriend@sina.com>
2019-11-29 20:34:51 +0800

    fix bug about argv in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 #include "help/help.h"
 
 int main(int argc, char** argv) {
@@ -6,11 +7,12 @@ int main(int argc, char** argv) {
 	switch (argc)
 	{
 	case 2:
-		if (argv[1] == "-h") {
+		if (!strcmp(argv[1],"-h")) {
 			getHelp();
 		}
 		else {
 			//TODO
+			std::cout << argv[1] << std::endl;
 		}
 		break;
 	case 3:
@@ -22,4 +24,5 @@ int main(int argc, char** argv) {
 		getHelp();
 		break;
 	}
+	return 0;
 }
