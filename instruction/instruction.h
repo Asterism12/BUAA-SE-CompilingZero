52b6331d336e2c15d232269811b73767c2d89e69
Qsort <awpfriend@sina.com>
2019-12-19 23:58:01 +0800

    update instruction.h

--- instruction/instruction.h
+++ instruction/instruction.h
@@ -16,6 +16,7 @@ public:
 	Instruction(Operation opr, std::int32_t x, std::int32_t y) : _opr(opr), _x(x), _y(y) {}
 	Instruction(Operation opr, int32_t x) : _opr(opr), _x(x), _y(0) {}
 	Instruction(Operation opr) : _opr(opr), _x(0), _y(0) {}
+	Instruction& operator=(Instruction i) { swap(*this, i); return *this; }
 	std::string getInstructionString() {
 		return "";
 	}
@@ -23,4 +24,11 @@ private:
 	Operation _opr;
 	std::int32_t _x;
 	std::int32_t _y;
+
+	inline void swap(Instruction& lhs, Instruction& rhs) {
+		using std::swap;
+		swap(lhs._opr, rhs._opr);
+		swap(lhs._x, rhs._x);
+		swap(lhs._y, rhs._y);
+	}
 };
